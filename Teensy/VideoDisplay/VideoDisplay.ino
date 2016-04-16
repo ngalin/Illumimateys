932cdf70da08c62388454c7a1c56f830c5f85574
Natalia Galin <natalia.galin@gmail.com>
2016-04-16 10:11:54 +1000

    Teensy code

--- Teensy/VideoDisplay/VideoDisplay.ino
+++ Teensy/VideoDisplay/VideoDisplay.ino
@@ -65,7 +65,7 @@
 // then goes right to left for its second row, then left to right,
 // zig-zagging for each successive row.
 #define LED_WIDTH      120   // number of LEDs horizontally
-#define LED_HEIGHT     32   // number of LEDs vertically (must be multiple of 8)
+#define LED_HEIGHT     24//32   // number of LEDs vertically (must be multiple of 8)
 #define LED_LAYOUT     0    // 0 = even rows left->right, 1 = even rows right->left
 
 // The portion of the video image to show on this set of LEDs.  All 4 numbers
