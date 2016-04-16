428b15c8d439f464e9ec6f4aa04ee8a144c2f751
Carlos Ledezma <cgledezma1101@gmail.com>
2016-04-16 17:10:59 +1000

    Point cloud transmission interfaces

---
+++ Kinect/ShadowWall/PointCloudWriter/HttpPointCloudWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowWall.PointCloudWriter
+{
+	public class HttpPointCloudWriter : IPointCloudWriter
+	{
+		public void WritePointCloud(IEnumerable<PointFrame> pointCloud, int width, int height)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Kinect/ShadowWall/PointCloudWriter/IPointCloudWriter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ShadowWall
+{
+	public interface IPointCloudWriter
+	{
+		void WritePointCloud(IEnumerable<PointFrame> pointCloud, int width, int height);
+	}
+}
