a025d7d5ff0f84d89a0c14461f99b7c98fd54a46
Carlos Ledezma <cgledezma1101@gmail.com>
2016-05-14 13:57:34 +1000

    Implmented filter to project a point with a light source behind it.

---
+++ Kinect/ShadowWall/Filters/LightSourceFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ShadowWall.Filters
+{
+	public class LightSourceFilter : IPointCloudFilter
+	{
+		public void Apply(IEnumerable<PointFrame> currentCloud)
+		{
+			foreach (var point in currentCloud)
+			{
+				var projectionSlope = (lightSourceHeight - point.Y) / (lightSourceDepth - point.Z);
+				point.Y = lightSourceHeight - (projectionSlope * lightSourceDepth);
+			}
+		}
+
+		#region Calibration
+
+		readonly int lightSourceDepth = 10;
+		readonly int lightSourceHeight = 2;
+
+		#endregion
+	}
+}
