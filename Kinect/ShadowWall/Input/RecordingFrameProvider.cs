1f29d2350a6bd5e11b8637d823edc79e997ad767
Carlos Ledezma <cgledezma1101@gmail.com>
2016-05-16 21:54:44 +1000

    Implemented a frame provider to load recordings

---
+++ Kinect/ShadowWall/Input/RecordingFrameProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShadowWall.Input
+{
+	public class RecordingFrameProvider : IFrameProvider
+	{
+		public RecordingFrameProvider(string fileName)
+		{
+			fileStream = File.OpenRead(fileName);
+			reader = new StreamReader(fileStream);
+
+			new Task(ProvideFrames).Start();
+		}
+
+		void ProvideFrames()
+		{
+			while (!reader.EndOfStream)
+			{
+				var frame = reader.ReadLine();
+				var depths = frame.Split(',').Select(depthStr => ushort.Parse(depthStr)).ToArray();
+				FrameArrived(null, new FrameArrivedArgs(depths, 640, 480));
+				Thread.Sleep(35);
+			}
+		}
+
+		public event EventHandler<FrameArrivedArgs> FrameArrived;
+
+		public void Dispose()
+		{
+			reader.Dispose();
+			fileStream.Dispose();
+		}
+
+		Stream fileStream;
+		StreamReader reader;
+	}
+}
