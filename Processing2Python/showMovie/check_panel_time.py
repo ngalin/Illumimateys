d2cc6d60703fafd06ab322b001c315d6612448ff
Natalia Galin <natalia.galin@gmail.com>
2016-05-26 13:07:46 +1000

    fixing start/stop panel times to vivid festival schedule

--- Processing2Python/showMovie/check_panel_time.py
+++ Processing2Python/showMovie/check_panel_time.py
@@ -1,12 +1,14 @@
 import datetime
 
-START_TIME_HOUR = 8
-END_TIME_HOUR = 22
+START_TIME_HOUR = 17 #5pm
+END_TIME_HOUR = 23 #11pm
 
+MINUTES_PAST_HOUR = 30
 
 def good_time_to_play():
     now = datetime.datetime.now()
-    if now.hour >= START_TIME_HOUR and now.hour < 22:
-        return True
+    if now.hour >= START_TIME_HOUR and now.hour < END_TIME_HOUR:
+        if now.minute > MINUTES_PAST_HOUR: #turn on at 5:30pm
+            return True
     else:
         return False
